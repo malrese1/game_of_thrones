89fe7d2662376f1e7770e2247dae2b47a1d712bf
Malcom Johnson <malcomjohnson@malcoms-MacBook-Pro.local>
2017-02-24 16:34:37 -0500

    2nd commit

--- got/db/seeds.rb
+++ got/db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+ # House.destroy_all
+ # Character.destroy_all
+
+ stark = House.create(name: 'Stark', region: 'North')
+ lannister = House.create(name: 'Lannister', region: 'South')
+
+ stark.characters.create(name: 'Arya')
+ stark.characters.create(name: 'Eddard')
+
+ lannister.characters.create(name: 'Tyrion')
+ lannister.characters.create(name: 'Cersei')
